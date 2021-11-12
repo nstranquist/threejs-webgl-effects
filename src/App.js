55f6ea961f22cc57626a4092501dfdeb5fd87077
Nico Stranquist <nstranquist@gmail.com>
2021-11-11 20:38:09 -0600

    add navbar for easy page switching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import styled from 'styled-components'
 import { Canvas } from '@react-three/fiber'
 import { useRecoilState } from 'recoil';
 import { pageState } from './atoms'
 import { GettingStarted } from './components/GettingStarted'
+
+const pages = ["getting-started"]
 
 function App() {
   const [page, setPage] = useRecoilState(pageState)
@@ -15,11 +18,56 @@ function App() {
     }
   }
 
+  const handleNavClick = (pageName) => {
+    setPage(pageName)
+  }
+
   return (
-    <div className="app-container">
+    <StyledApp className="app-container">
+      <div className="page-nav-bar">
+        {pages.map((pageName, index) => (
+          <button className="nav-button" onClick={() => handleNavClick(pageName)} key={`${index}-${pageName}`}>
+            {pageName}
+          </button>
+        ))}
+      </div>
       {renderPage()}
-    </div>
+    </StyledApp>
   );
 }
+
+const StyledApp = styled.div`
+  &.app-container {
+    position: relative;
+    min-height: 100vh;
+
+    .page-nav-bar {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 60px;
+    }
+
+    .nav-button {
+      cursor: pointer;
+      margin-left: 5px;
+      margin-right: 5px;
+      padding: 6px 12px;
+      border-radius: 4px;
+      border: none;
+      outline: none;
+      background: white;
+      transition: .07s ease-in-out;
+
+      &:hover {
+        background: rgba(33,33,33,.08);
+        transition: .11s ease-in-out;
+      }
+    }
+  }
+  #canvas-container {
+
+  }
+`
 
 export default App;
