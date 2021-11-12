ae627856c451b32fcb4411637cdc85cae5c64b1a
Nico Stranquist <nstranquist@gmail.com>
2021-11-11 22:39:42 -0600

    added three testing library and wrote 2 simple tests that fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import ReactThreeTestRenderer from '@react-three/test-renderer'
+import { RotatingBox } from './components/SpringAnimations'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('mesh to have two children', async () => {
+  const renderer = await ReactThreeTestRenderer.create(<RotatingBox />)
+
+  const mesh = renderer.scene.children[0].allChildren
+
+  expect(mesh.length).toBe(2)
+})
+
+test('click event makes box bigger', async () => {
+  const renderer = await ReactThreeTestRenderer.create(<RotatingBox />)
+
+  const mesh = renderer.scene.children[0]
+
+  await renderer.fireEvent(mesh, 'click')
+
+  expect(mesh.props.scale).toBe(1.5)
+})
