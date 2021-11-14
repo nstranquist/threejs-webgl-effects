f31465f98c68a32d91341d53baf2d58aafc7d523
Nico Stranquist <nstranquist@gmail.com>
2021-11-14 15:58:39 -0600

    complete virtual world example

--- src/components/VirtualWorld.js
+++ src/components/VirtualWorld.js
@@ -1,18 +1,24 @@
 import niceColors from 'nice-color-palettes';
 import * as THREE from 'three'
+import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { Box, Plane } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
-import { Suspense, useMemo } from 'react'
+import { Suspense, useMemo, useState } from 'react'
 import { Physics, useBox, usePlane, useSphere } from "@react-three/cannon";
 import styled from 'styled-components'
 
 // reference: https://www.section.io/engineering-education/create-a-3d-world-in-react-using-three.js-react-three-fiber/
 
 export const VirtualWorld = () => {
-
+  const [numberOfSpheres, setNumberOfSpheres] = useState(1)
 
   return (
-    <div id="canvas-container">
+    <StyledVirtualWorld id="canvas-container">
+      <div className="controls-container">
+        <p># Spheres: {numberOfSpheres}</p>
+        <button onClick={() => setNumberOfSpheres(prev => prev + 1)}>Add 1</button>
+        <button onClick={() => setNumberOfSpheres(prev => prev > 0 ? prev - 1 : prev)}>Remove 1</button>
+      </div>
       <Canvas camera={{ position: [0, 0, 0], near: 0.1, far: 1000 }}>
         {/*
           // Set gravity
@@ -34,6 +40,11 @@ export const VirtualWorld = () => {
           <PhyBox position={[2, 0, -5]} />
           <PhyBox position={[0, 0, -5]} />
           <PhyBox position={[-2, 0, -5]} />
+
+          <Suspense fallback={null}>
+            <InstancedSpheres number={numberOfSpheres} />
+          </Suspense>
+
         </Physics>
 
         {/* // This is for provision of ambient lighting in the scene */}
@@ -44,12 +55,29 @@ export const VirtualWorld = () => {
 
         {/* // Apart from ambient light and point light, you can add others such as fog */}
       </Canvas>
-    </div>
+    </StyledVirtualWorld>
   )
 }
 
 const StyledVirtualWorld = styled.div`
+  position: relative;
 
+  .controls-container {
+    position: absolute;
+    right: 0;
+    top: 0;
+    padding: 20px;
+    background: transparent;
+    display: flex;
+    flex-direction: column;
+    
+    button {
+      /* border: none;
+      outline: none;
+      background: rgba(255,255,255,.75);
+      color: black; */
+    }
+  }
 `
 
 // It allows you to input a color as an argument
@@ -94,4 +122,43 @@ function PhyBox(props) {
         <meshNormalMaterial />
       </Box>
   );
+}
+
+// This  is used to create spherical objects in the app
+// 'carbon_normal.jpg' is used as a texture loader for each one of them
+// The mass is 1, while the start position; that is, when the app is started is obtained randomly in the x and y axis
+// We shall use some blocks of code to setup the color of each sphere so that each sphere may look differently
+function InstancedSpheres({ number }) {
+  const map = useLoader(TextureLoader, '/images/carbon_normal.jpg')
+  const [ref] = useSphere(index => ({
+    mass: 1,
+    position: [Math.random() - 0.5, Math.random() - 0.5, index * 4],
+    args:1
+  }))
+  const colors = useMemo(() => {
+    const array = new Float32Array(number * 3)
+    const color = new THREE.Color()
+    for (let i = 0; i < number; i++)
+      color
+          .set(niceColors[17][Math.floor(Math.random() * 5)])
+          .convertSRGBToLinear()
+          .toArray(array, i * 3)
+    return array
+  }, [number])
+  return (
+      <instancedMesh ref={ref} castShadow receiveShadow args={[null, null, number]}>
+        <sphereBufferGeometry attach="geometry" args={[1, 16, 16]}>
+          <instancedBufferAttribute attachObject={['attributes', 'color']} args={[colors, 3]} />
+        </sphereBufferGeometry>
+        <meshPhongMaterial
+            attach="material"
+            vertexColors={THREE.VertexColors}
+            normalMap={map}
+            normalScale={[1, 1]}
+            normalMap-wrapS={THREE.RepeatWrapping}
+            normalMap-wrapT={THREE.RepeatWrapping}
+            normalMap-repeat={[10, 10]}
+        />
+      </instancedMesh>
+  )
 }
