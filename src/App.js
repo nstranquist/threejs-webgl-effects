6a49cbef40b126af93f7c137014a24c2f92bf85a
Nico Stranquist <nstranquist@gmail.com>
2021-11-24 15:06:42 -0600

    add rotating rocket / burger effect. ignore all .glb files

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ const pages = [
   "camera-scroll",
   "sprite-as-3d",
   "router-transitions",
+  "rotating-rocket",
 ]
 
 function App() {
@@ -75,6 +76,9 @@ function App() {
       case "router-transitions":
         const RouterTransitions = lazy(() => import('./components/RouterTransitions/RouterTransitions'))
         return <RouterTransitions />
+      case "rotating-rocket":
+        const RotatingRocket = lazy(() => import('./components/RotatingRocket'))
+        return <RotatingRocket />
       default:
         return <div><h1>No Page Selected</h1><p><button onClick={() => setPage("getting-started")}>Reset</button></p></div>
     }
---
+++ src/assets/CONVERSION.md
@@ -0,0 +1,15 @@
+# Converting Assets
+
+GLB and gLTF are the most popular.
+
+I have already run into several issues converting to and from these formats.
+
+Here is one good idea to set up:
+`npm i -g gltf-pipeline`
+
+Usage:
+`gltf-pipeline -i <source file> -o <output file>`
+`gltf-pipeline -i shoe.gltf -o shoe.glb`
+
+See More: https://blog.logrocket.com/configure-3d-models-react-three-fiber/
+
